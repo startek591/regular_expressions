e3b4fc335b0016651a892ebbe720ae6da0e99973
Jonathan Fambro <startek591@gmail.com>
2023-10-17 11:07:07 -0400

    Wild Card Period

--- wildcard.js
+++ wildcard.js
@@ -1,5 +1,6 @@
-let text = "What is that? and why it is hot.";
+let text = `What is that? and why it is hot h?t h!t h t hoot h
+t h t h.t.`;
 
-let regex1 = /h./g;
+let regex1 = /h.t/g;
 
 console.log(text.match(regex1));
