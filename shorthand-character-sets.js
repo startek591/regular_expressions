d0c2d2982abed744cb08b51067ac270be911c15c
Jonathan Fambro <startek591@gmail.com>
2023-10-17 15:34:47 -0400

    Shortcut of digits

---
+++ shorthand-character-sets.js
@@ -0,0 +1,11 @@
+let text = `A String that contains numbers (12345)
+   A second line _ (12345)`;
+
+let regex1 = /\d/g;
+
+console.log(text.match(regex1));
+
+// shortcuts
+// \d -> [0-9]
+// \w -> [a-zA-Z0-9_]
+// \s -> [\t\r\n]
