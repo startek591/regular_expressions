ab8883c0e9721c13174deeefce82187ba17c5295
Jonathan Fambro <startek591@gmail.com>
2023-10-17 10:38:01 -0400

    Using exec method with flags

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const str = "Hello world. HiS This is Leela";
 
 const regex = new RegExp("s", "gi");
 
-console.log(str.match(regex));
+console.log(regex.exec(str));
+console.log(regex.exec(str));
+console.log(regex.exec(str));
+console.log(regex.exec(str));
