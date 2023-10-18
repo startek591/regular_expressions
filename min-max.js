4d5d87797e95fba5c3ec5daf9fd34a126e16f35d
Jonathan Fambro <startek591@gmail.com>
2023-10-18 12:07:06 -0400

    Example of minimum and more occurrences

--- min-max.js
+++ min-max.js
@@ -1,9 +1,9 @@
 let text = `My telephone number is as follows 801-555-6789`;
 
-let regex1 = /\w{3,5}/g;
+let regex1 = /\w{3,}/g;
 
 console.log(text.match(regex1));
 
 // {min, max} => matches min to max
 // {min} => min occurrence
-// {min,} => min
+// {min,} => min or more
