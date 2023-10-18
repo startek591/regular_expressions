201796e5b5b9f876332258345390607c9fd7fda9
Jonathan Fambro <startek591@gmail.com>
2023-10-17 20:33:58 -0400

    laziness nature

--- greediness_laziness.js
+++ greediness_laziness.js
@@ -1,6 +1,6 @@
 let text = `<p>This is the first paragraph</p><p>This is the second paragraph</p>
 <p>This is the first paragraph</p><p>This is the first paragraph</p>`;
 
-let regex1 = /<p>.*<\/p>/g;
+let regex1 = /<p>.*?<\/p>/g;
 
 console.log(text.match(regex1));
